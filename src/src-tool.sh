202b455d58f40f534177e780f86580ba3184edbc
YanZhao <zhaoyan.hrb@gmail.com>
2017-07-02 00:15:29 -0500

    f***

--- src/src-tool.sh
+++ src/src-tool.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
 ctags -R .
-find . -name '*.cpp' > cscope.files
+if [ ! -f ./cscope.files ]; then
+		    echo "cscope.files not found!"
+find . -name '*.cpp' -or -name '*.cxx' > cscope.files
+fi 
 cscope -Rbq
-doxygen -g
+
+cd ..
+if [ ! -f ./Doxyfile ]; then
+		    echo "Doxyfile not found!"
+	doxygen -g
+	echo "GENERATE_LATEX = NO" >> Doxyfile
+	echo "INPUT = ./src" >> Doxyfile
+	                                                     
+fi
+
+cd ./src
+if [ ! -f ./.ycm_extra_conf.py ]; then
+		    echo "ycm not found!"
+			~/.vim/bundle/YCM-Generator/config_gen.py .
+fi
